92002be0b5bcd13967cac1b7b297f166df497281
GuyJeff1 <isaac_peck@mines.edu>
2026-04-01 22:29:44 -0600

    Programmed task 2
    Added file ready for feedback from @maddox

--- pair_programming.py
+++ pair_programming.py
@@ -1,21 +1,10 @@
-# Maddox: 2
-# Isaac: 3
+array = input("list:").split(",") #takes a list of numbers as input and seperates them by commas
+offset = float(input("offset:")) #takes a number as input as sets it as the offset
+val = 0 #sets the val variable to 0
+for i in array: #iterates through the array and adds each number together
+    i = float(i)
+    val += i
 
-
-# 1) Takes two values (feet and inches) and converts to meters
-#freaky_feet = float(input("Enter feet: "))
-#winches = float(input("Enter inches: "))
-#meters = (freaky_feet * 0.3048) + (winches * 0.0254)
-#print(f"{freaky_feet} ft {winches} in is equal to {meters} m.")
-
-
-# 2) Changes coordinates from polar to Cartesian
-from cmath import cos, sin
-r = float(input("Enter radius: "))
-teeta = float(input("Enter angle in deg: "))
-teeta_rad = teeta*(3.14159/180) # this is da formula theta*pi/180deg which is used to convert deg to rad
-x = r*cos(teeta_rad) # polar to cart formula
-y = r*sin(teeta_rad) # also polar to cart formula
-print(f"The polar coordinates (r={r}, θ={teeta}°) are (x={x}, y={y}) in cartesian coordinates.")
-
-
+mean = val / len(array) #divides the total val by the length of the array to get the mean
+new_mean = mean + offset #adds the offset to the mean to get a new mean
+print(new_mean) #prints the new mean
